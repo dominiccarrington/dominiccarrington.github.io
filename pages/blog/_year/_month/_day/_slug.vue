3dbcd8951ea011530b93ae7e0e1e4d709295d50f
Dominic Carrington <carringtondominic@gmail.com>
2019-09-09 14:46:11 +0100

    Added tags to articles

--- pages/blog/_year/_month/_day/_slug.vue
+++ pages/blog/_year/_month/_day/_slug.vue
@@ -3,7 +3,8 @@
     <h2>{{ blog.title }}</h2>
     <section class="h6">
       <small>Published On: {{ $moment(blog.iso8601Date).format("DD/MM/YYYY HH:mm") }}</small><br>
-      <small>Last Edited On: {{ $moment(blog.mtime).format("DD/MM/YYYY HH:mm") }}</small>
+      <small>Last Edited On: {{ $moment(blog.mtime).format("DD/MM/YYYY HH:mm") }}</small><br>
+      <small v-if="blog.tags.length > 0 && blog.tags[0] != null">Tagged: {{ blog.tags.join(", ") }}</small>
     </section>
     <section class="blog-content" v-html="$md.render(blog.content)"></section>
   </article>
@@ -16,6 +17,7 @@ export default {
     const slug = `${params.year}-${params.month}-${params.day}-${params.slug}`;
     const blogposts = await app.$axios.$get("/blogposts.json");
     const blog = blogposts[slug];
+    blog.tags.sort();
 
     return {
       blog
