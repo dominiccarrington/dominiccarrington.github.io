2d8fb01c0cb3e96b25dfc9c6530b69578a230048
Dominic Carrington <carringtondominic@gmail.com>
2019-11-21 22:22:56 +0000

    Change graphs on update of data

--- components/Chart/DoughnutChart.vue
+++ components/Chart/DoughnutChart.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import { mixins } from 'vue-class-component';
 import { Doughnut } from 'vue-chartjs';
 import { ChartData, ChartOptions } from 'chart.js';
@@ -8,6 +8,14 @@ import { ChartData, ChartOptions } from 'chart.js';
 export default class BarChart extends mixins(Doughnut) {
   @Prop({ type: Object, required: true }) readonly data!: ChartData;
   @Prop({ type: Object, required: true }) readonly options!: ChartOptions;
+
+  @Watch('data')
+  @Watch('options')
+  changeChart() {
+    this.$data._chart.data = this.data;
+    this.$data._chart.options = this.options;
+    this.$data._chart.update();
+  }
 
   mounted() {
     this.renderChart(this.data, this.options);
