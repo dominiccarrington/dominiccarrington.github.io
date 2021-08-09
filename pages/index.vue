74e0e80803ce6b0425b16f68c6f1aa236bee12fe
Dominic <carringtondominic@gmail.com>
2021-08-09 10:25:45 +0100

    Added coming soon page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-<div>Hello World</div>
-
+  <div class="flex w-full h-full justify-center items-center">
+    <div class="text-center">
+      <h1 class="text-5xl font-bold">Welcome!</h1>
+      <h2 class="text-2xl">This site is currently being built</h2>
+    </div>
+  </div>
 </template>
+
+<style>
+html, body, #__nuxt, #__layout {
+  height: 100%;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
